a98f28e715956f48f8650bfa81c00eacbda1a03a
31211995 <esra2.yossef@gmail.com>
2024-03-13 14:56:22 +0400

    accordion section

--- src/app/Components/Accordion.jsx
+++ src/app/Components/Accordion.jsx
@@ -1,12 +1,13 @@
 "use client";
 import React from "react";
 import { faqs } from "@/constants";
-import Item from "./Item";
+
 import { useState } from "react";
+import { Container } from "postcss";
 
 const Accordion = () => {
-  const [active, setActive] = useState();
-  console.log(active);
+  const [active, setActive] = useState(null);
+
   const toggle = (index) => {
     if (active === index) {
       setActive(null);
@@ -15,17 +16,33 @@ const Accordion = () => {
   };
 
   return (
-    <div>
-      <div>
-        {faqs.map((data, i) => (
-          <Item
-            key={i}
-            question={data.question}
-            answer={data.answer}
-            onClick={() => toggle(i)}
-            active={active}
-            index={i}
-          />
+    <div className="  ">
+      <div className="w-[1420px] mr-[135px] ">
+        {faqs.map((data, index) => (
+          <newContainer
+            key={index} onClick={() => toggle(index)
+            }>
+                <div className="flex justify-between w-[920px] mt-10 ml-[225px] items-center  cursor-pointer">
+        <p className="font-bold font-poppins text-white  text-[24px] ">
+          {data.question}
+        </p>
+        <span className=" text-white  text-[24px]">{active===index?  "-"  :"+"}</span>
+      </div>
+      <div
+        className={`overflow-hidden h-0 ${
+          active === index
+            ? "h-auto transition-all duration-75 ease-[cubic-bezier(0,1,0,1)] "
+            : ""
+        }`}
+      >
+        <p className="w-[920px] text-gray text-poppins text-[16px] ml-[225px] my-[20px] tracking-wide ">
+          {data.answer}
+        </p>
+        <div className="h-[1px] bg-gray w-[920px] ml-[225px] mt-[20px]"></div>
+       
+      </div>
+      
+            </newContainer>
         ))}
       </div>
     </div>
--- src/app/Components/Item.jsx
+++ src/app/Components/Item.jsx
@@ -2,24 +2,24 @@
 
 const Item = (props) => {
   return (
-    <div>
-      <div className="flex justify-between w-[1020px] mt-10 ml-[225px] items-center ">
+    <div className="mb-[10px] w-[920px]  ">
+      <div className="flex justify-between w-[920px] mt-10 ml-[225px] items-center  cursor-pointer">
         <p className="font-bold font-poppins text-white  text-[24px] ">
           {props.question}
         </p>
         <span className=" text-white  text-[24px]">+</span>
       </div>
-      <div
-        className={` h-0 overflow-hidden ${
-          props.active === props.index
-            ? " transition-all duration-75 ease-[cubic-bezier(0,1,0,1)] "
+     
+        <div  className={`  h-0 overflow-hidden transition-all duration-75 ease-[cubic-bezier(0,1,0,1)]  ${
+          props.active ===props.index
+            ? "max-h-[9999px] transition-all duration-75 ease-[cubic-bezier(1,0,1,0)] bg-red-400"
             : ""
-        }`}
-      >
-        <p className="w-[920px] text-gray text-poppins text-[16px] ml-[225px] mt-[1px] tracking-wide ">
+        }`}>
+        <p className=" text-gray text-poppins text-[16px] ml-[225px] mt-[1px] tracking-wide ">
           {props.answer}
         </p>
-      </div>
+        </div>
+      
     </div>
   );
 };
